30e2cecbd5fd474de4bd1e6aa013298b4a009f10
Jeff Brimager <jbrimager@chef.io>
2020-05-07 22:59:47 -0500

    adding os constants
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec')
+const os = require('os')
 
 async function main() {
   try {
     // Get the variables we care about
-    const os = core.getInput('os')
+    // const os = core.getInput('os')
     const suite = core.getInput('suite')
     const action = core.getInput('action')
     var suiteName = ''
@@ -19,13 +20,16 @@ async function main() {
 
     if (os.platform() != 'win32')
     {
-
-      await exec.exec(`powershell.exe -command "kitchen ${action} ${suiteName}"`)
-      core.addPath(`${windowsPath}\\bin`)
-      // core.addPath(`${windowsPath}\\embedded\\bin`)
+      await exec.exec(`kitchen ${action} ${suiteName}`)
     }
     else
+    const windowsPath = core.getInput('windowsPath');
+      if (version) {
+        versionParam = `-version ${version}`
+      }
+      else
     await exec.exec(`kitchen ${action} ${suiteName}`)
+    core.addPath(`${windowsPath}\\bin`)
   } catch (error){
     core.setFailed(error.message);
   }
