a041379a6df42ba69bc68f677a5f0563751dbfba
Jeff Brimager <jbrimager@chef.io>
2020-05-08 07:49:50 -0500

    adding input for is variable
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec')
-const os = require('os')
+const OS = require('os')
 
 async function main() {
   try {
     // Get the variables we care about
-    // const os = core.getInput('os')
+    const os = core.getInput('os')
     const suite = core.getInput('suite')
     const action = core.getInput('action')
     var suiteName = ''
     if (suite)
     {
-       suiteName += `${suite}`
+       suiteName += suite
     }
     if (os)
     {
       suiteName += `-${os}`
     }
 
-    if (os.platform() != 'win32')
+    if (OS.platform() != 'win32')
     {
       await exec.exec(`kitchen ${action} ${suiteName}`)
     }
