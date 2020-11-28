29a305091078bfc02af27514c36bc1ae3b5b6f3f
Jason Field <jason@avon-lea.co.uk>
2020-11-28 13:48:36 +0000

    Add Deprection warning for master

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const exec = require('@actions/exec')
 
 async function main() {
   try {
+    core.warning("::warning ::Github Action: actionshub/chef-install has migrated to the main branch as default, the master branch will be removed")
+
     // Get the variables we care about
     const os = core.getInput('os')
     const suite = core.getInput('suite')
