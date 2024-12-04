fabbda767d6a6e2866fcc056307c7211a5635583
Dan Webb <dan.webb@damacus.io>
2024-12-04 16:36:31 +0000

    Update dependencies (#23)
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const core = require('@actions/core');
 const exec = require('@actions/exec')
 
 async function main() {
-  try {
-    // Get the variables we care about
-    const os = core.getInput('os')
-    const suite = core.getInput('suite')
-    const action = core.getInput('action')
-    var suiteName = ''
-    if (suite)
-    {
-       suiteName += suite
-    }
-    if (os)
-    {
-      suiteName += `-${os}`
-    }
+	try {
+		// Get the variables we care about
+		const os = core.getInput('os')
+		const suite = core.getInput('suite')
+		const action = core.getInput('action')
+		var suiteName = ''
+		if (suite) {
+			suiteName += suite
+		}
+		if (os) {
+			suiteName += `-${os}`
+		}
 
-    await exec.exec(`kitchen ${action} ${suiteName}`)
-  } catch (error){
-    core.setFailed(error.message);
-  }
+		await exec.exec(`kitchen ${action} ${suiteName}`)
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 main()
