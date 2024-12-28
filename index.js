939c105d7257a960e788afa50dada6833f31ea33
Dan Webb <dan.webb@damacus.io>
2024-12-28 06:07:02 +0000

    feat: Add ability to specify a working directory (#25)
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec')
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 async function main() {
-	try {
-		// Get the variables we care about
-		const os = core.getInput('os')
-		const suite = core.getInput('suite')
-		const action = core.getInput('action')
-		var suiteName = ''
-		if (suite) {
-			suiteName += suite
-		}
-		if (os) {
-			suiteName += `-${os}`
-		}
+  try {
+    // Get the variables we care about
+    const os = core.getInput("os");
+    const suite = core.getInput("suite");
+    const action = core.getInput("action");
+    const workingDirectory = core.getInput("working-directory");
 
-		await exec.exec(`kitchen ${action} ${suiteName}`)
-	} catch (error) {
-		core.setFailed(error.message);
-	}
+    var suiteName = "";
+    if (suite) {
+      suiteName += suite;
+    }
+    if (os) {
+      suiteName += `-${os}`;
+    }
+
+    const options = {
+      cwd: workingDirectory,
+    };
+
+    await exec.exec(`kitchen ${action} ${suiteName}`, [], options);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
-main()
+main();
